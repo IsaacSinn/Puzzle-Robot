fc9aa15a02131eae15d07c97f1ec8136bbd97cfb
IsaacSinn <sinweitengisaac@gmail.com>
2023-03-31 02:30:56 -0400

    segment_color added minimum rect box function and crops image

--- segment_color.py
+++ segment_color.py
@@ -2,11 +2,6 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
-# import images
-solved = cv.imread('./Puzzle Data/Phone Cam/black mat/solution.jpg')
-piece = cv.imread('./Puzzle Data/Phone Cam/black mat/piece_resize.jpg')
-assert piece is not None, "file could not be read, check with os.path.exists()"
-assert solved is not None, "file could not be read, check with os.path.exists()"
 
 
 # Parameters 
@@ -16,47 +11,94 @@ GAUSSIAN_BLUR = (9,9)
 MIN_AREA_CONTOUR = 100
 
 
-def identify_contour(imshow = True):
-    piece_blur = cv.GaussianBlur(piece, GAUSSIAN_BLUR, 0)
-    piece_hsv = cv.cvtColor(piece_blur, cv.COLOR_BGR2HSV)
+def identify_contour(img, imshow = False):
+    img_blur = cv.GaussianBlur(img, GAUSSIAN_BLUR, 0)
+    img_hsv = cv.cvtColor(img_blur, cv.COLOR_BGR2HSV)
 
     # segmentation using lower and upper bound hsv values
-    piece_seg = cv.inRange(piece_hsv, HSV_LOWER, HSV_UPPER)
-    piece_seg = 255 - piece_seg
+    img_seg = cv.inRange(img_hsv, HSV_LOWER, HSV_UPPER)
+    img_seg = 255 - img_seg
 
     # clean segmentation image
     kernel = np.ones((5,5),np.uint8)
-    piece_seg_cleaned = cv.morphologyEx(piece_seg, cv.MORPH_CLOSE, kernel)
+    img_seg_cleaned = cv.morphologyEx(img_seg, cv.MORPH_CLOSE, kernel)
 
-    # detect contours and largest contour
-    contours, heirarchy = cv.findContours(piece_seg_cleaned, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    largest_contour = sorted(contours, key=cv.contourArea, reverse=True)[0]
+    # detect contours
+    contours, heirarchy = cv.findContours(img_seg_cleaned, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
     # filter contours that are inside other contours
     filtered_contours = []
     for i, contour in enumerate(contours):
         if heirarchy[0][i][3] == -1 and cv.contourArea(contour) > MIN_AREA_CONTOUR:
             filtered_contours.append(contour)
+
+    largest_contour = sorted(filtered_contours, key=cv.contourArea, reverse=True)[0]
     
-    # draw contours
-    piece_contour = piece.copy()
-    cv.drawContours(piece_contour, [largest_contour], -1, (0,255,0), 3)
-    piece_contour = cv.cvtColor(piece_contour, cv.COLOR_BGR2RGB) # change to RGB (matplotlib uses RGB)
-
-
-    # show images
-    fig, axs = plt.subplots(1,3, figsize = (20,10))
-    axs[0].imshow(piece_seg, cmap = 'gray')
-    axs[0].set_title("segmented piece")
-    axs[1].imshow(piece_seg_cleaned, cmap = 'gray')
-    axs[1].set_title("cleaned segment image")
-    axs[2].imshow(piece_contour)
-    axs[2].set_title("contours")
+    # draw contours and minimum bounding rectangle
+    img_contour = img.copy()
+    cv.drawContours(img_contour, [largest_contour], -1, (0,255,0), 3)
+    img_contour = cv.cvtColor(img_contour, cv.COLOR_BGR2RGB) # change to RGB (matplotlib uses RGB)
 
     if imshow:
+        # show images
+        fig, axs = plt.subplots(1,3, figsize = (10,5))
+        axs[0].imshow(img_seg, cmap = 'gray')
+        axs[0].set_title("segmented image")
+        axs[1].imshow(img_seg_cleaned, cmap = 'gray')
+        axs[1].set_title("cleaned segmentation")
+        axs[2].imshow(img_contour)
+        axs[2].set_title("contours")
+
+
+        plt.show()
+    
+    return img, largest_contour
+
+def minimum_cropped(img, contour, imshow = True):
+    
+    # find the minimum box rect of the contour
+    rect = cv.minAreaRect(contour)
+    box = cv.boxPoints(rect)
+    box = np.int0(box)
+
+
+    (x, y) = (box[1])
+    w = max(box[2][0], box[3][0]) - min(box[0][0], box[1][0])
+    h = max(box[0][1], box[3][1]) - min(box[1][1], box[2][1])
+    img_cropped = img[y:y+h, x:x+w]
+
+    # draw bounding box
+    img_box = img.copy()
+    cv.drawContours(img_box, [box], -1, (0,255,0), 3)
+    img_box = cv.cvtColor(img_box, cv.COLOR_BGR2RGB)
+
+    if imshow:
+        # show images
+        fig, axs = plt.subplots(1,3, figsize = (10,5))
+        axs[0].imshow(img_box)
+        axs[0].set_title("image box contour")
+        img_cropped_RGB = cv.cvtColor(img_cropped, cv.COLOR_BGR2RGB)
+        axs[1].imshow(img_cropped_RGB)
+        axs[1].set_title("image cropped")
         plt.show()
 
-identify_contour()
+    return img_cropped
+
+def main():
+
+    # import images
+    solution = cv.imread('./Puzzle Data/Phone Cam/black mat/solution.jpg')
+    piece = cv.imread('./Puzzle Data/Phone Cam/black mat/piece_resize.jpg')
+    assert piece is not None, "file could not be read, check with os.path.exists()"
+    assert solution is not None, "file could not be read, check with os.path.exists()"
+
+    
+    _, contour = identify_contour(piece)
+    cropped_piece = minimum_cropped(piece, contour)
+
+
+if __name__ == "__main__":
+    main()
 
 
 
