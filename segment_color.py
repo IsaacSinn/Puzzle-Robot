f3db26f46483263322e2362c9a95633fd312cafd
IsaacSinn <sinweitengisaac@gmail.com>
2023-04-01 04:11:57 -0400

    Todo: debug cropping with pts

--- segment_color.py
+++ segment_color.py
@@ -4,14 +4,14 @@ from matplotlib import pyplot as plt
 
 
 
-# Parameters 
-HSV_LOWER = np.array([6,17,0]) #HSV
-HSV_UPPER = np.array([35,115,210])
-GAUSSIAN_BLUR = (9,9)
-MIN_AREA_CONTOUR = 100
-
-
 def identify_contour(img, imshow = False):
+    
+    # Parameters 
+    HSV_LOWER = np.array([6,17,0]) #HSV
+    HSV_UPPER = np.array([35,115,210])
+    GAUSSIAN_BLUR = (9,9)
+    MIN_AREA_CONTOUR = 100
+
     img_blur = cv.GaussianBlur(img, GAUSSIAN_BLUR, 0)
     img_hsv = cv.cvtColor(img_blur, cv.COLOR_BGR2HSV)
 
@@ -33,13 +33,14 @@ def identify_contour(img, imshow = False):
             filtered_contours.append(contour)
 
     largest_contour = sorted(filtered_contours, key=cv.contourArea, reverse=True)[0]
-    
-    # draw contours and minimum bounding rectangle
-    img_contour = img.copy()
-    cv.drawContours(img_contour, [largest_contour], -1, (0,255,0), 3)
-    img_contour = cv.cvtColor(img_contour, cv.COLOR_BGR2RGB) # change to RGB (matplotlib uses RGB)
 
     if imshow:
+
+        # draw contours and minimum bounding rectangle
+        img_contour = img.copy()
+        cv.drawContours(img_contour, [largest_contour], -1, (0,255,0), 3)
+        img_contour = cv.cvtColor(img_contour, cv.COLOR_BGR2RGB) # change to RGB (matplotlib uses RGB)
+
         # show images
         fig, axs = plt.subplots(1,3, figsize = (10,5))
         axs[0].imshow(img_seg, cmap = 'gray')
@@ -54,32 +55,53 @@ def identify_contour(img, imshow = False):
     
     return img, largest_contour
 
-def minimum_cropped(img, contour, imshow = True):
+def minimum_cropped(img, contour, imshow = False):
     
     # find the minimum box rect of the contour
     rect = cv.minAreaRect(contour)
-    box = cv.boxPoints(rect)
-    box = np.int0(box)
+    box_original = cv.boxPoints(rect)
+    box_original = np.int0(box_original)
 
+    # get angle and center of rect
+    angle = rect[2]
+    if angle > 45:
+        angle = 90 - angle
+    center = rect[0]
 
-    (x, y) = (box[1])
-    w = max(box[2][0], box[3][0]) - min(box[0][0], box[1][0])
-    h = max(box[0][1], box[3][1]) - min(box[1][1], box[2][1])
-    img_cropped = img[y:y+h, x:x+w]
+    # create a rotation matrix and rotate the image around the center point
+    M = cv.getRotationMatrix2D(center, angle, 1)
+    rotated = cv.warpAffine(img, M, (img.shape[1], img.shape[0]))
 
-    # draw bounding box
-    img_box = img.copy()
-    cv.drawContours(img_box, [box], -1, (0,255,0), 3)
-    img_box = cv.cvtColor(img_box, cv.COLOR_BGR2RGB)
+    # get box points and rotate box
+    pts = np.int0(cv.transform(np.array([box_original]), M))[0]
+    pts[pts < 0] = 0
+    print(box_original)
+    print(pts)
+
+    # cropping image
+    img_cropped = rotated[pts[1][1]:pts[0][1], pts[1][0]:pts[2][0]]
 
     if imshow:
+        # draw bounding box for original
+        img_box_original = img.copy()
+        cv.drawContours(img_box_original, [box_original], -1, (0,255,0), 3)
+        img_box_original = cv.cvtColor(img_box_original, cv.COLOR_BGR2RGB)
+
+        # draw bounding box for rotated
+        img_box_rotate = rotated.copy()
+        cv.drawContours(img_box_rotate, [pts], -1, (0,255,0), 3)
+        img_box_rotate = cv.cvtColor(img_box_rotate, cv.COLOR_BGR2RGB)
+
+        # change cropped img to RGB (for matplotlib)
+        img_cropped_RGB = cv.cvtColor(img_cropped, cv.COLOR_BGR2RGB)
+
         # show images
         fig, axs = plt.subplots(1,3, figsize = (10,5))
-        axs[0].imshow(img_box)
-        axs[0].set_title("image box contour")
-        img_cropped_RGB = cv.cvtColor(img_cropped, cv.COLOR_BGR2RGB)
-        axs[1].imshow(img_cropped_RGB)
-        axs[1].set_title("image cropped")
+        axs[0].imshow(img_box_original)
+        #axs[0].set_title("image box contour")
+        axs[1].imshow(img_box_rotate)
+        #axs[1].set_title("image cropped")
+        axs[2].imshow(img_cropped_RGB)
         plt.show()
 
     return img_cropped
@@ -92,9 +114,22 @@ def main():
     assert piece is not None, "file could not be read, check with os.path.exists()"
     assert solution is not None, "file could not be read, check with os.path.exists()"
 
-    
-    _, contour = identify_contour(piece)
-    cropped_piece = minimum_cropped(piece, contour)
+    # identify contour and crop puzzle piece
+    # _, contour = identify_contour(piece, imshow=True)
+    # cropped_piece = minimum_cropped(piece, contour, imshow=True)
+
+    # identify contour and crop solution puzzle
+    _, solution_contour = identify_contour(solution, imshow=True)
+    cropped_solution = minimum_cropped(solution, solution_contour, imshow= True)
+
+    # # generate image
+    # img = np.zeros((1000, 1000), dtype=np.uint8)
+    # img = cv.line(img,(400,400),(511,511),(255,255,255),120)
+    # img = cv.line(img,(300,300),(700,500),(255,255,255),120)
+
+    # # find contours / rectangle
+    # contours,_= cv.findContours(img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    # crop = minimum_cropped(img, contours[0], imshow=True)
 
 
 if __name__ == "__main__":
